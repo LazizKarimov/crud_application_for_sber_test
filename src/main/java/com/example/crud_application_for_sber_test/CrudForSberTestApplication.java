e7f47ad264dfe9d1508475a488d56e23ac90cda0
MacBook <https://github.com/LazizKarimov>
2024-02-22 17:22:51 +0300

    Init commit

---
+++ src/main/java/com/example/crud_application_for_sber_test/CrudForSberTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_application_for_sber_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudForSberTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudForSberTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud_application_for_sber_test/CrudApplicationForSberTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_application_for_sber_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationForSberTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
